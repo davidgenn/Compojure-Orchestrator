5ec0a5cc256c66154f5fd92b918a0aaaec22fda3
David Genn <davidgenn@gmail.com>
2015-04-26 18:46:53 +0100

    Filter headers to include

--- src/orchestrator/handler.clj
+++ src/orchestrator/handler.clj
@@ -9,30 +9,38 @@
 
 (defn generate-response-body [response-future]
   {:status (:status @response-future)
-   :body (:body @response-future)}
-  )
+   :body (:body @response-future)})
 
 (defn add-to-response [response-map response-future]
   (let [response-key (get response-future 0)
         response-promise (get response-future 1)]
     (conj response-map {response-key (generate-response-body response-promise)})))
 
-(defn submit-single-request [result parameter]
-  (conj result {(get parameter 0) (http/get (get parameter 1))}))
+(defn submit-single-request-with-headers [headers result parameter]
+  (let [options {:timeout 1000 :headers headers}]
+    (println options)
+    (conj result {(get parameter 0) (http/get (get parameter 1) options)})))
 
-(defn submit-all-requests [params]
-  (reduce submit-single-request {} params))
+(defn submit-single-request [headers]
+  (partial submit-single-request-with-headers headers))
+
+(defn submit-all-requests [params headers]
+  (let [submit-function (submit-single-request headers)]
+    (reduce submit-function {} params)))
 
 (defn build-response [futures]
   (reduce add-to-response {} futures))
 
-(defn get-multiple-urls [params]
+(defn get-multiple-urls [params headers]
   (let
-    [futures (submit-all-requests params)]
+    [futures (submit-all-requests params headers)]
     (response (build-response futures))))
 
+(defn filter-headers [headers]
+  (select-keys headers ["CST" "X-SECURITY-TOKEN"]))
+
 (defroutes app-routes
-           (GET "/orchestrate" {params :query-params} (get-multiple-urls params))
+           (GET "/orchestrate" {params :query-params headers :headers} (get-multiple-urls params (filter-headers headers)))
            (route/not-found "Not Found"))
 
 (def app
